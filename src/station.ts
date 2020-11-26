ca12fe7d331a7110bbe2f102b6bea6387e44ca79
Laurent Curau <laurent@thecodeshed.dev>
2020-11-26 18:29:23 +0100

    A station has a set of platforms

--- src/station.ts
+++ src/station.ts
@@ -1,6 +1,8 @@
 import { StationCode } from "./station-code"
+import { Platform } from "./platform"
 
 export interface Station {
     readonly uid: StationCode
     readonly stopTime: number
+    readonly platforms: readonly Platform[]   
 }
