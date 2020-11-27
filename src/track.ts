1c9ae44b1354d1270f9f5a6cb50618331a3b5dcc
Laurent Curau <laurent@thecodeshed.dev>
2020-11-27 15:29:42 +0100

    Track specifies > 0 int number of trains

--- src/track.ts
+++ src/track.ts
@@ -1,5 +1,7 @@
+import { PositiveInteger } from "./positive-integer";
 import { Station } from "./station"
 
 export interface Track {
     readonly endpointUids: readonly [Station, Station]
+    readonly capacity: PositiveInteger
 }
