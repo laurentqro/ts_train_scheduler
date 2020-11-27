26126232071ceb6176a05e0528a17f7b9967c05f
Laurent Curau <laurent@thecodeshed.dev>
2020-11-27 15:25:25 +0100

    Station stop time is positive integer

--- src/station.ts
+++ src/station.ts
@@ -1,8 +1,9 @@
 import { StationCode } from "./station-code"
 import { Platform } from "./platform"
+import { PositiveInteger } from "./positive-integer"
 
 export interface Station {
     readonly uid: StationCode
-    readonly stopTime: number
+    readonly stopTime: PositiveInteger
     readonly platforms: readonly Platform[]   
 }
