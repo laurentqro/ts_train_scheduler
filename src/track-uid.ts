24c0c215166a1e55f074559ed0cfdee9baac633a
Laurent Curau <laurent@thecodeshed.dev>
2020-11-28 14:05:52 +0100

    Track uid must be unique

--- src/track-uid.ts
+++ src/track-uid.ts
@@ -1,18 +1,38 @@
 export class TrackUid {
-    private value: string
+    private static key(value: string): string {
+        return value.toString()
+    }
 
-    constructor(value: string) {
+    private static instances: {
+        [key: string]: TrackUid | undefined
+    } = {}
+
+    private constructor(private value: string) {
         const regex = RegExp('^[0-9]{9}$')
         let isValid = regex.test(value)
 
         if (!isValid) {
             throw Error("Track UID must be exactly nine decimal digits")
         }
-
-        this.value = value
     }
 
     toString(): string {
         return this.value.toString()
+    }
+
+    public static new(value: string): TrackUid {
+        let key = TrackUid.key(value)
+        let instance = TrackUid.instances[key]
+
+        if (instance) {
+            throw Error("Track with this UID already exists")
+        }
+
+        if (instance == undefined) {
+            instance = new TrackUid(value)
+            TrackUid.instances[key] = instance
+        }
+
+        return instance
     }
 }
--- tests/track-uid.test.ts
+++ tests/track-uid.test.ts
@@ -1,12 +1,17 @@
 import { TrackUid } from "../src/track-uid"
 
 test('track is identified by a string of exactly nine decimal digits', () => {
-    expect(() => new TrackUid("123")).toThrow(Error)
-    expect(() => new TrackUid("ABCDEFGHI")).toThrow(Error)
-    expect(() => new TrackUid("123456789")).not.toThrow(Error)
+    expect(() => TrackUid.new("123")).toThrow(Error)
+    expect(() => TrackUid.new("ABCDEFGHI")).toThrow(Error)
+    expect(() => TrackUid.new("123456789")).not.toThrow(Error)
 })
 
 test('toString', () => {
-    let trackUid = new TrackUid("123456789")
-    expect(`${trackUid}`).toEqual("123456789")
+    let trackUid = TrackUid.new("987654321")
+    expect(`${trackUid}`).toEqual("987654321")
 });
+
+test('track uid is unique', () => {
+    TrackUid.new("098098098")
+    expect(() => TrackUid.new("098098098")).toThrow(Error)
+})
