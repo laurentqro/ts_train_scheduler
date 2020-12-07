be955431ce66e9d86ea12c1142f1846ac4907a8c
Laurent Curau <laurent@thecodeshed.dev>
2020-12-07 18:17:00 +0100

    Station endpointUids are StationUid

--- src/track.ts
+++ src/track.ts
@@ -1,10 +1,10 @@
 import { PositiveInteger } from "./positive-integer";
-import { Station } from "./station"
+import { StationUid } from "./station-uid"
 import { TrackUid } from "./track-uid";
 
 export interface Track {
     readonly uid: TrackUid
-    readonly endpointUids: readonly [Station, Station]
+    readonly endpointUids: readonly [StationUid, StationUid]
     readonly capacity: PositiveInteger
     readonly maxSpeed: PositiveInteger
     readonly distance: PositiveInteger
