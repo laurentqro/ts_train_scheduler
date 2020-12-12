38fe85fc3ec4a08c1965557d23f4b40ac2bcc1f2
Laurent Curau <laurent@thecodeshed.dev>
2020-12-12 18:02:01 +0100

    Remove ServiceUid (outside of application scope)
    The ServiceUid is generated during the bidding phase, which is outside
    of the scope of this application.

--- src/service-bid.ts
+++ src/service-bid.ts
@@ -3,13 +3,13 @@ import { ServiceUid } from "./service-uid";
 import { Stop } from "./stop";
 
 export class ServiceBid {
-    readonly uid: ServiceUid
+    readonly uid: string
     readonly operatorUid: OperatorUid
     readonly stops: readonly [Stop, Stop, ...Stop[]]
     readonly bidAmount: number
 
-    constructor(operatorUid: OperatorUid, stops: [Stop, Stop, ...Stop[]], bidAmount: number) {
-        this.uid = new ServiceUid()
+    constructor(uid: string, operatorUid: OperatorUid, stops: [Stop, Stop, ...Stop[]], bidAmount: number) {
+        this.uid = uid
         this.operatorUid = operatorUid
         this.stops = stops
         this.bidAmount = bidAmount
--- tests/service-uid.test.ts
+++
@@ -1,9 +0,0 @@
-import { ServiceUid } from "../src/service-uid"
-
-test('service UID is incremental', () => {
-    let serviceUid1 = new ServiceUid()
-    let serviceUid2 = new ServiceUid()
-
-    expect(serviceUid1.value).toEqual(1)
-    expect(serviceUid2.value).toEqual(2)
-})
