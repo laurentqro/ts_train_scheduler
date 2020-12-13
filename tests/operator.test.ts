5ee1df1e449852e6b78d5d6b38d4b36b70ae1f63
Laurent Curau <laurent@thecodeshed.dev>
2020-12-13 03:44:49 +0100

    Add TrainUid

--- tests/operator.test.ts
+++ tests/operator.test.ts
@@ -1,10 +1,11 @@
 import { Operator } from "../src/operator"
 import { OperatorUid } from "../src/operator-uid"
 import { PositiveInteger } from "../src/positive-integer"
+import { TrainUid } from "../src/train-uid"
 
 test('each train is uniquely identified within its operator', () => {
-    let train1 = { uid: "123456", length: PositiveInteger.new(5n) }
-    let train2 = { uid: "123456", length: PositiveInteger.new(3n) }
+    let train1 = { uid: new TrainUid("123456"), length: PositiveInteger.new(5n) }
+    let train2 = { uid: new TrainUid("123456"), length: PositiveInteger.new(3n) }
 
     expect(() => new Operator(OperatorUid.new("ABC"), [train1, train2])).toThrow(Error)
 
