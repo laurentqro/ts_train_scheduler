5656a9a61e8d9590b21953219572a4278bc26239
Laurent Curau <laurent@thecodeshed.dev>
2020-12-13 03:46:40 +0100

    Load network data only once for all tests

--- tests/network.test.ts
+++ tests/network.test.ts
@@ -1,24 +1,25 @@
-
 import { Network } from "../src/network";
 import { NetworkLoader } from "../src/network-loader";
 import { Operator } from "../src/operator";
 import { OperatorUid } from "../src/operator-uid";
 import { Station } from "../src/station";
 import { StationUid } from "../src/station-uid";
-import * as data from "./fixtures/network.json";
+import * as data from "./fixtures/network.json"
 
-test('get operator by UID', () => {
+describe('The network', () => {
     let network: Network = NetworkLoader.loadNetwork(data)
-    let operatorUid: OperatorUid = OperatorUid.new("ABC")
-    let operator: Operator | null = network.getOperator(operatorUid)
 
-    expect(`${operator?.uid}`).toEqual("ABC")
-})
+    test('get operator by UID', () => {
+        let operatorUid: OperatorUid = OperatorUid.new("ABC")
+        let operator: Operator | null = network.getOperator(operatorUid)
 
-test('get station by UID', () => {
-    let network: Network = NetworkLoader.loadNetwork(data)
-    let stationUid: StationUid = StationUid.new("LHR")
-    let station: Station | null = network.getStation(stationUid)
+        expect(`${operator?.uid}`).toEqual("ABC")
+    })
 
-    expect(`${station?.uid}`).toEqual("LHR")
+    test('get station by UID', () => {
+        let stationUid: StationUid = StationUid.new("LHR")
+        let station: Station | null = network.getStation(stationUid)
+
+        expect(`${station?.uid}`).toEqual("LHR")
+    })
 })
